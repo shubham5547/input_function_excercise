23b1a8fed675b97c64c659a58679245383c1796b
shubham5547 <64001725+shubham5547@users.noreply.github.com>
2020-05-23 18:57:40 +0530

    Add files via upload

---
+++ input_excercise1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Ask user name and print back user name in reverse order
+# note: try to make your program in 2 lines using string formatting
+
+
+# In[3]:
+
+
+name = input("enter your name ")
+print(name[::-1])
+
+
+# In[18]:
+
+
+# using string formatting python3.6 version
+name = input("enter your name : ")
+print(f" reverse of your name is {(name[::-1])}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
